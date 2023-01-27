22d82c0c3b5b143d42b35161c54afd0a32c80df3
nguyenha <nguyenhbhcm@gmail.com>
2023-01-27 16:24:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ def main(config_path):
     with open(config_path, "r") as config:
         config = yaml.safe_load(config)
     
-    geom_path = config['downloader']['geom']
-    PLANET_API_KEY = config['downloader']['key']
-    list_quad_URL = config['downloader']['list_quad_url']
-    dates = config['downloader']['dates']
-    bbox = config['downloader']['bbox']
-    quads_path = config['downloader']['quads_path']
+    geom_path = config['geom']
+    PLANET_API_KEY = config['key']
+    list_quad_URL = config['list_quad_url']
+    dates = config['dates']
+    bbox = config['bbox']
+    quad_dir = config['quad_dir']
+    tile_dir = config['tile_dir']
+    temp_dir = config['temp_dir']
+    tilefile_path = config['tilefile_path']
 
-    aoi = gpd.read_file(config['downloader']['geom'])[['geometry']].dissolve()
-    downloader = PlanetDownloader()
+    aoi = gpd.read_file(config['geom'])[['geometry']].dissolve()
+    downloader = PlanetDownloader(geom_path = geom_path)
     quads_gdf, quads_url = downloader.get_basemap_grid (
                                             geom_path = geom_path, 
                                             PLANET_API_KEY = PLANET_API_KEY, 
@@ -23,10 +26,13 @@ def main(config_path):
                                             aoi = aoi,
                                             bbox = bbox
                                         )
-    downloader.download_tiles(
-        quads_path, PLANET_API_KEY, quads_gdf = quads_gdf, 
-        download_url = quads_url, list_quad_URL = list_quad_URL, dates = dates, bbox = bbox
-    )
+    if config['doDownload']:
+        downloader.download_tiles(
+            quad_dir, PLANET_API_KEY, quads_gdf = quads_gdf, 
+            download_url = quads_url, list_quad_URL = list_quad_URL, dates = dates, bbox = bbox
+        )
+    if config['doRetile']:
+        errors = downloader.retiler(tile_dir, quad_dir, temp_dir, tilefile_path, dates, quads_gdf)
 
 if __name__ =='__main__':
     main('config/config.yml')
